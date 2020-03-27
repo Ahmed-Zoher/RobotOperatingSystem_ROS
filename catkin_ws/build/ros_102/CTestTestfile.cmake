f6d14215533c92c2f6fa63d03e766cdcaa815e56
Ahmed-Zoher <ahmed.o.zoher@gmail.com>
2020-03-27 21:33:16 +0200

    initial Creation Signed-off-by: Ahmed-Zoher <ahmed.o.zoher@gmail.com>

---
+++ catkin_ws/build/ros_102/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/catkin_ws/src/ros_102
+# Build directory: /home/ahmed/catkin_ws/build/ros_102
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
